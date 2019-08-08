b5b83297364be11c56f423991b23653eab11b759
fromSpace <starsnic@gmail.com>
2019-08-08 13:38:58 -0500

    declarative shadow roots are not the way to go, goals changed

--- src/custom_elements/ez2heart-hello-element.js
+++ src/custom_elements/ez2heart-hello-element.js
@@ -4,19 +4,17 @@ var register = require("@skatejs/ssr/register");
 const render = require("@skatejs/ssr");
 
 class Hello extends HTMLElement {
-    constructor() {
-      super();
-      // A shadow root might already exists if it's been
-      // rendered on the server and you load this on the
-      // client.
-      if (!this.shadowRoot) {
-        this.attachShadow({ mode: "open" }).innerHTML = `
-          Hello, <slot></slot>!
-        `;
-      }
+  constructor() {
+    super();
+    // A shadow root might already exists if it's been
+    // rendered on the server and you load this on the
+    // client.
+    if (!this.shadowRoot) {
+      this.attachShadow({ mode: "open" }).innerHTML = `
+        Hello, <slot></slot>!
+      `;
     }
   }
-  customElements.define("x-hello", Hello);
-  const hello = new Hello();
-  hello.textContent = "World";
-  render(hello).then(console.log);
+}
+customElements.define("x-hello", Hello);
+export default Hello;
